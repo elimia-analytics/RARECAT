cd39ce8fecaf818db5ec3152587442db68763026
Giovanni Rapacciuolo <147450162+elimia-analytics@users.noreply.github.com>
2025-02-21 09:59:36 -0800

    Added RARECAT.exe executable to run app locally

---
+++ RARECAT.R
@@ -0,0 +1,21 @@
+#' Please do not rename this file !
+## Identify required package names
+### CRAN packages
+cran_packages <- c("shiny", "leaflet", "leaflet.extras", "purrr", "shinyjs", "sf", "terra", 
+                   "dplyr", "plotly", "htmltools", "htmlwidgets", "shinyWidgets", "sortable", 
+                   "shinydashboard", "shinycssloaders", "shinyBS", "DT", "natserv", "rgbif", "flexdashboard", 
+                   "shinybusy", "leafpm", "red", "dygraphs", "RWmisc", "units", "shiny.exe")
+installed_cran_packages <- cran_packages %in% rownames(installed.packages())
+if (any(installed_cran_packages == FALSE)) {
+  install.packages(cran_packages[!installed_cran_packages])
+}
+library(shiny.exe)
+hostUnix(
+    appDir = '/Users/giorap/Library/CloudStorage/Dropbox/elimia/code/apps/RARECAT',
+    port = getOption('shiny.port'),
+    launch.browser = TRUE,
+    host = '127.0.0.1',
+                  workerId = '',
+                  quiet = FALSE,
+                  display.mode = c('auto', 'normal', 'showcase'),
+                  test.mode = getOption('shiny.testmode', FALSE))
